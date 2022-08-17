75c5797f0bf0b5ab6d839a0a53676bb1bb669b84
Joey Pettit <pettitbass@gmail.com>
2022-08-16 22:33:56 -0500

    Fixed 'needs improvement' issues

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -3,13 +3,18 @@ console.log('***** Cart Functions *****');
 // We want to see how you are testing your code!!!
 
 
+// ~~~~~ Global Variables~~~~~
+
 let basket = [];
+const maxItems = 5;
 
 
-
+//~~~~~~~~~~~~~
 function addItem(item){
     if (typeof item !== 'string') {
        console.log("incorrect data type, please input a string") ;
+    } else if (isFull() === true){
+        console.log('Your basket is already full');
     } else {
         basket.push(item);
         return true;
@@ -26,6 +31,8 @@ console.log("in my basket is: " + basket);
 
 
 
+//~~~~~~~~~~~
+
 
 function listItems(){
     for(let item of basket){
@@ -34,3 +41,92 @@ function listItems(){
 }
 
 listItems();
+
+
+//~~~~~~~~~~~~
+
+
+
+console.log('This is basket before empty(): ' + basket);
+
+function empty(){
+    for(let i = 0; i = basket.length;   ){
+        basket.pop();
+    }
+}
+
+empty();
+console.log('This is basket AFTER empty(): ' + basket);
+
+
+
+
+
+//          ~~~~~ Stretch Goals ~~~~~
+
+
+
+
+function isFull(){
+    if (basket.length < maxItems){
+        return false;
+    } else {
+        return true;
+    }
+}
+
+addItem("beans");
+addItem("carrots");
+addItem("tuna");
+addItem("coffee");
+console.log("Test of isFull(), should be false: ", isFull());
+addItem("peanuts");
+console.log("Test of isFull(), should be true: ", isFull());
+addItem("ketchup");
+
+
+//~~~~~~~~~~~~
+
+
+
+function removeItem(item) { 
+    if (typeof item !== 'string') {
+        console.log("incorrect data type, please input a string") ;
+    } else {
+        let removedIndex = basket.indexOf(item);
+        console.log("This should show index of item removed: ", removedIndex);
+        if(removedIndex === -1){
+            return "This should say null: ", null;
+        } else {
+            let removed = basket.splice(removedIndex, 1);
+            console.log("this should show item removed: ", removed)
+            return removed.toString();
+        }
+    }
+}
+
+console.log("This is removeItem Test:")
+console.log(basket);
+console.log("This should be say tuna: ", removeItem("tuna"));
+console.log(basket);
+console.log(removeItem("bacon"));
+console.log(basket);
+
+/*
+
+
+
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
